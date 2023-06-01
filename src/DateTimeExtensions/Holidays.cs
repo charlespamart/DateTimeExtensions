7d54383a92f781a05821238dc8fce4e8c4270efd
charles.pamart <charles.pamart@totalazurecloud.onmicrosoft.com>
2023-06-01 20:19:42 +0200

    Updated IsSunday, IsHoliday, IsHolidayOrSunday to extension method.
    Added Cache for holidays to avoid instancing the object every time.

--- src/DateTimeExtensions/Holidays.cs
+++ src/DateTimeExtensions/Holidays.cs
@@ -2,28 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DateTimeExtensions
+namespace DateTimeExtensions;
+
+public static class Holidays
 {
-    public static class Holidays
+    private static readonly Dictionary<int, IReadOnlyCollection<DateTime>> HolidaysCache = new();
+
+    public static bool IsSunday(this DateTime dateTime) => dateTime.DayOfWeek == DayOfWeek.Sunday;
+
+    public static bool IsHoliday(this DateTime dateTime) =>
+        AllHolidays(dateTime.Year).Any(holiday => holiday.Date == dateTime.Date);
+
+    public static bool IsHolidayOrSunday(this DateTime dateTime) => IsSunday(dateTime) || IsHoliday(dateTime);
+
+    public static DateTime Easter(int year)
     {
-        public static bool IsSunday(DateTime dateTime)
-        {
-            return dateTime.DayOfWeek == DayOfWeek.Sunday;
-        }
+        var (month, day) = ExecuteEasterAlgorithm(year);
 
-        public static bool IsHoliday(DateTime dateTime)
-        {
-            return AllHolidays(dateTime.Year).Any(holiday => DateTime.Compare(holiday, dateTime) == 0);
-        }
+        return new DateTime(year, month, day);
+    }
 
-        public static bool IsHolidayOrSunday(DateTime dateTime)
-        {
-            return IsSunday(dateTime) || IsHoliday(dateTime);
-        }
+    public static DateTime Ascension(int year) => Easter(year).AddDays(38);
 
-        private static IReadOnlyCollection<DateTime> AllHolidays(int year)
+    public static DateTime Whit(int year) => Easter(year).AddDays(49);
+
+    public static DateTime NewYear(int year) => new(year, 1, 1);
+
+    public static DateTime Labor(int year) => new(year, 5, 1);
+
+    public static DateTime WorldWarTwo(int year) => new(year, 5, 8);
+
+    public static DateTime Bastille(int year) => new(year, 7, 14);
+
+    public static DateTime AssumptionOfMary(int year) => new(year, 8, 15);
+
+    public static DateTime AllSaints(int year) => new(year, 11, 1);
+
+    public static DateTime Armistice(int year) => new(year, 11, 11);
+
+    public static DateTime Christmas(int year) => new(year, 12, 25);
+
+    private static IReadOnlyCollection<DateTime> AllHolidays(int year)
+    {
+        if (!HolidaysCache.ContainsKey(year))
         {
-            return new List<DateTime>()
+            HolidaysCache[year] = new List<DateTime>()
             {
                 Easter(year),
                 Ascension(year),
@@ -39,43 +62,22 @@ namespace DateTimeExtensions
             };
         }
 
-        public static DateTime Easter(int year)
-        {
-            var (month, day) = ExecuteEasterAlgorithm(year);
+        return HolidaysCache[year];
+    }
 
-            return new DateTime(year, month, day);
-        }
+    // This method implements the "Anonymous Gregorian algorithm" for calculating the date of Easter
+    // The algorithm was submitted to the journal Nature in 1876 and has been reprinted many times
+    // It is also called “Meeus/Jones/Butcher” algorithm
+    // Source: https://en.wikipedia.org/wiki/Computus#Anonymous
+    private static (int month, int day) ExecuteEasterAlgorithm(int year)
+    {
+        var g = (year / 100 - (year / 100 + 8) / 25 + 1) / 3;
+        var h = (19 * (year % 19) + year / 100 - year / 100 / 4 - g + 15) % 30;
+        var l = (32 + 2 * (year / 100 % 4) + 2 * (year % 100 / 4) - h - year % 100 % 4) % 7;
+        var m = (year % 19 + 11 * h + 22 - l) / 451;
+        var month = (h + l - 7 * m + 114) / 31;
+        var day = (h + l - 7 * m + 114) % 31 + 2;
 
-        public static DateTime Ascension(int year) => Easter(year).AddDays(38);
-
-        public static DateTime Whit(int year) => Easter(year).AddDays(49);
-
-        public static DateTime NewYear(int year) => new(year, 1, 1);
-
-        public static DateTime Labor(int year) => new(year, 5, 1);
-
-        public static DateTime WorldWarTwo(int year) => new(year, 5, 8);
-
-        public static DateTime Bastille(int year) => new(year, 7, 14);
-
-        public static DateTime AssumptionOfMary(int year) => new(year, 8, 15);
-
-        public static DateTime AllSaints(int year) => new(year, 11, 1);
-
-        public static DateTime Armistice(int year) => new(year, 11, 11);
-
-        public static DateTime Christmas(int year) => new(year, 12, 25);
-
-        private static (int month, int day) ExecuteEasterAlgorithm(int year)
-        {
-            int g = (year / 100 - (year / 100 + 8) / 25 + 1) / 3;
-            int h = (19 * (year % 19) + year / 100 - year / 100 / 4 - g + 15) % 30;
-            int l = (32 + 2 * (year / 100 % 4) + 2 * (year % 100 / 4) - h - year % 100 % 4) % 7;
-            int m = (year % 19 + 11 * h + 22 - l) / 451;
-            int month = (h + l - 7 * m + 114) / 31;
-            int day = (h + l - 7 * m + 114) % 31 + 2;
-
-            return (month, day);
-        }
+        return (month, day);
     }
 }
--- tests/DateTimeExtensions.Tests/HolidaysShould.cs
+++ tests/DateTimeExtensions.Tests/HolidaysShould.cs
@@ -6,73 +6,82 @@ namespace DateTimeExtensions.Tests
     public class HolidaysShould
     {
         [Theory]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2021), MemberType = typeof(DateTimeExtensionsHelpers))]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2022), MemberType = typeof(DateTimeExtensionsHelpers))]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2023), MemberType = typeof(DateTimeExtensionsHelpers))]
-        public void ReturnsTrueOnHoliday(int year, int month, int day)
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2021),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2022),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2023),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        public void ReturnTrueOnHoliday(int year, int month, int day)
         {
             var dateTime = new DateTime(year, month, day);
-            
-            var sut = Holidays.IsHoliday(dateTime);
+
+            var sut = dateTime.IsHoliday();
 
             Assert.True(sut);
         }
 
         [Theory]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetNoHolidayDates), MemberType = typeof(DateTimeExtensionsHelpers))]
-        public void ReturnsFalseOnNonHoliday(int year, int month, int day)
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetNoHolidayDates),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        public void ReturnFalseOnNonHoliday(int year, int month, int day)
         {
             var dateTime = new DateTime(year, month, day);
-            
-            var sut = Holidays.IsHoliday(dateTime);
+
+            var sut = dateTime.IsHoliday();
 
             Assert.False(sut);
         }
 
         [Theory]
         [MemberData(nameof(DateTimeExtensionsHelpers.GetSundayDates), MemberType = typeof(DateTimeExtensionsHelpers))]
-        public void ReturnsTrueOnSunday(int year, int month, int day)
+        public void ReturnTrueOnSunday(int year, int month, int day)
         {
             var dateTime = new DateTime(year, month, day);
-            
-            var sut = Holidays.IsSunday(dateTime);
+
+            var sut = dateTime.IsSunday();
 
             Assert.True(sut);
         }
 
         [Theory]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetNoHolidayDates), MemberType = typeof(DateTimeExtensionsHelpers))]
-        public void ReturnsFalseOnNotSunday(int year, int month, int day)
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetNoHolidayDates),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        public void ReturnFalseOnNotSunday(int year, int month, int day)
         {
             var dateTime = new DateTime(year, month, day);
-            
-            var sut = Holidays.IsSunday(dateTime);
+
+            var sut = dateTime.IsSunday();
 
             Assert.False(sut);
         }
-        
+
         [Theory]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2021), MemberType = typeof(DateTimeExtensionsHelpers))]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2022), MemberType = typeof(DateTimeExtensionsHelpers))]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2023), MemberType = typeof(DateTimeExtensionsHelpers))]
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2021),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2022),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetHolidayDatesIn2023),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
         [MemberData(nameof(DateTimeExtensionsHelpers.GetSundayDates), MemberType = typeof(DateTimeExtensionsHelpers))]
-        public void ReturnsTrueOnSundayOrHoliday(int year, int month, int day)
+        public void ReturnTrueOnSundayOrHoliday(int year, int month, int day)
         {
             var dateTime = new DateTime(year, month, day);
-            
-            var sut = Holidays.IsHolidayOrSunday(dateTime);
-            
+
+            var sut = dateTime.IsHolidayOrSunday();
+
             Assert.True(sut);
         }
-        
+
         [Theory]
-        [MemberData(nameof(DateTimeExtensionsHelpers.GetNoHolidayDates), MemberType = typeof(DateTimeExtensionsHelpers))]
-        public void ReturnsFalseOnNonSundayOrNonHoliday(int year, int month, int day)
+        [MemberData(nameof(DateTimeExtensionsHelpers.GetNoHolidayDates),
+            MemberType = typeof(DateTimeExtensionsHelpers))]
+        public void ReturnFalseOnNonSundayOrNonHoliday(int year, int month, int day)
         {
             var dateTime = new DateTime(year, month, day);
-            
-            var sut = Holidays.IsHolidayOrSunday(dateTime);
-            
+
+            var sut = dateTime.IsHolidayOrSunday();
+
             Assert.False(sut);
         }
 
@@ -80,10 +89,10 @@ namespace DateTimeExtensions.Tests
         [InlineData(2021)]
         [InlineData(2022)]
         [InlineData(2023)]
-        public void ReturnsEasterDayForAGivenYear(int year)
+        public void ReturnEasterDayForAGivenYear(int year)
         {
             var sut = Holidays.Easter(year);
-            
+
             var expected = DateTimeExtensionsHelpers.GetEasterDateByYear[year];
 
             Assert.Equal(expected.Year, sut.Year);
@@ -95,10 +104,10 @@ namespace DateTimeExtensions.Tests
         [InlineData(2021)]
         [InlineData(2022)]
         [InlineData(2023)]
-        public void ReturnsAscensionDayForAGivenYear(int year)
+        public void ReturnAscensionDayForAGivenYear(int year)
         {
             var sut = Holidays.Ascension(year);
-            
+
             var expected = DateTimeExtensionsHelpers.GetAscensionDateByYear[year];
 
             Assert.Equal(expected.Year, sut.Year);
@@ -110,10 +119,10 @@ namespace DateTimeExtensions.Tests
         [InlineData(2021)]
         [InlineData(2022)]
         [InlineData(2023)]
-        public void ReturnsWhitDayForAGivenYear(int year)
+        public void ReturnWhitDayForAGivenYear(int year)
         {
             var sut = Holidays.Whit(year);
-            
+
             var expected = DateTimeExtensionsHelpers.GetWhitDateByYear[year];
 
             Assert.Equal(expected.Year, sut.Year);
